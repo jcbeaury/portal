358b664eebf40d32974283d68a6052eb3df102d8
jcbeaury <jcbeaury@gmail.com>
2014-07-14 15:42:46 -0400

    Add supervision to the portals
    When a portal shuts down, it will be recreated.
    Data does not persist when this happens.

--- lib/portal.ex
+++ lib/portal.ex
@@ -1,19 +1,14 @@
 defmodule Portal do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
     children = [
-      # Define workers and child supervisors to be supervised
-      # worker(Portal.Worker, [arg1, arg2, arg3])
+      worker(Portal.Door, [])
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Portal.Supervisor]
+    opts = [strategy: :simple_one_for_one, name: Portal.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
@@ -45,6 +40,13 @@ defmodule Portal do
 
     # Return the portal itself
     portal
+  end
+
+  @doc """
+  Shoots a new door with the given 'color'.
+  """
+  def shoot(color) do
+    Supervisor.start_child(Portal.Supervisor, [color])
   end
 end
 
